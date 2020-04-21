094790112d85797bc59353ea5f0139970629c6f1
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-04-21 19:40:57 -0400

    updated files to mvp

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken"); // ----> npm i jsonwebtoken
+const jwt = require("jsonwebtoken");
 
 const Users = require("../users-model");
 const secrets = require("../secrets");
@@ -30,15 +30,12 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
-  // search for the user using the username
   Users.findBy({ username })
     .then(([user]) => {
-      // if we find the user, then also check that passwords match
+      // checks if user is found, if so, also checks if passwords match
       if (user && bcrypt.compareSync(password, user.password)) {
-        // produce a token
         const token = generateToken(user);
 
-        // send the token to the client
         res.status(200).json({ message: "Welcome!", token });
       } else {
         res.status(401).json({ message: "bro, log in..." });
@@ -50,6 +47,7 @@ router.post("/login", (req, res) => {
     });
 });
 
+//function that creates a token
 function generateToken(user) {
   // the data
   const payload = {
